381d66bb653d2e853f8200e1bd8d8d10e2a740c8
Jose Carlos Vilchez <josecarlos.vilchez@unidadeditorial.es>
2022-02-08 09:06:05 +0100

    Updates keyboard

--- src/Framework/index.tsx
+++ src/Framework/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import RouteService from "./Service/RouteService";
-import './style.scss';
+import './style.sass';
 
 ReactDOM.render(
     <BrowserRouter>
