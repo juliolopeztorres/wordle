91b5f8cec17702a00f5653231ed20ae9f13708ee
Jose Carlos Vilchez <josecarlos.vilchez@unidadeditorial.es>
2022-02-09 14:31:35 +0100

    Adds mui alerts instead js alert

--- src/Framework/View/DefaultView.tsx
+++ src/Framework/View/DefaultView.tsx
@@ -1,7 +1,13 @@
 import React, { Component, ReactNode } from "react";
 import { hot } from "react-hot-loader/root";
 import wordslib from '../../Data/wordslib.json';
-import Button from '@material-ui/core/Button';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import ClearAllIcon from '@mui/icons-material/ClearAll';
+import BackspaceIcon from '@mui/icons-material/Backspace';
+import SendIcon from '@mui/icons-material/Send';
+import ReplaySharpIcon from '@mui/icons-material/ReplaySharp';
+import Alert from '@mui/material/Alert';
 
 const TODAY_WORD = wordslib[Math.floor(Math.random()*wordslib.length)]
 
@@ -9,7 +15,8 @@ class DefaultView extends Component {
   state = {
     words: [[], [], [], [], []],
     sent: [],
-    solved: false
+    solved: false,
+    notValidWord: false
   }
 
   // @ts-ignore
@@ -50,28 +57,26 @@ class DefaultView extends Component {
   }
 
   onSpecialKeyClicked(sendOrDelete: string) {
-    const {words, solved} = this.state
-
+    const {words, solved} = this.state;
     if (solved) {
-      return false
+      return false;
     }
-
     let currentIndex = this.getCurrentIndex()
     if (sendOrDelete === 'ENVIAR') {
-
       if (words[currentIndex].length !== 5) {
-        return
+        return;
       }
-
       this.setState((prevState) => {
         // @ts-ignore
-        const sent = prevState.sent
+        const sent = prevState.sent;
 
         const proposed = words[currentIndex].join('')
 
         if (!wordslib.includes(proposed)) {
-          alert('La palabra no está en el diccionario')
-          return prevState
+          return {
+            ...prevState, 
+            notValidWord: true
+          };
         }
 
         // @ts-ignore
@@ -83,8 +88,7 @@ class DefaultView extends Component {
           solved: proposed === TODAY_WORD
         }
       })
-
-      return
+      return;
     }
 
     if(currentIndex === -1) {
@@ -103,6 +107,20 @@ class DefaultView extends Component {
         words: words
       }
     })
+  }
+
+  clearCurrentWord() {
+    const {words} = this.state;
+    if (words[this.getCurrentIndex()].length === 5) {
+      this.setState((prevState) => {
+        words[this.getCurrentIndex()] = [];
+        return {
+          ...prevState,
+          words: words,
+          notValidWord: false
+        }
+      })
+    }
   }
 
   canSendLetter() {
@@ -134,10 +152,11 @@ class DefaultView extends Component {
   }
 
   render(): ReactNode {
-    const {words, sent} = this.state
+    const {words, sent, notValidWord, solved} = this.state
 
-    const KeyboardButton = (props: { letter: string, color: string }) => <Button
+    const KeyboardButton = (props: { letter: string, disabled: boolean }) => <Button
       variant="outlined"
+      disabled={props.disabled}
       onClick={() => {
         const letter = props.letter.toUpperCase()
         if(['ENVIAR', 'BORRAR'].includes(letter)){
@@ -159,9 +178,7 @@ class DefaultView extends Component {
           this.onSpecialKeyClicked('ENVIAR');
           return
       }}>
-      <svg version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-        <path d="M15.5 4.4C9.5 7.2 6.8 10 4.1 16 .5 23.9 2.2 34.3 8.3 40.9 11.6 44.5 19.8 48 25 48c5.2 0 13.4-3.5 16.7-7.1 3.4-3.7 6.3-11 6.3-15.9 0-5.2-3.5-13.4-7.1-16.7C37.2 4.9 29.9 2 25 2c-2.7 0-6.5 1-9.5 2.4zM31 16.5c2.9 3 5.1 5.8 4.7 6.2-.4.4-2.8-1.3-5.2-3.7L26 14.6v12.2c0 7.5-.4 12.2-1 12.2s-1-4.7-1-12.2V14.6L19.5 19c-2.4 2.4-4.8 4.1-5.2 3.7C13.7 22 23.7 11 25 11c.3 0 3 2.5 6 5.5z"/>
-      </svg>
+      <SendIcon />
     </Button>
 
     const DeleteButton = () => <Button
@@ -170,18 +187,16 @@ class DefaultView extends Component {
         this.onSpecialKeyClicked('BORRAR');
         return
       }}>
-      <svg version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-        <path d="M4.8 9.2C.8 14.6.7 14.8 4 19.5l3.1 4.6 10.7-.3 10.7-.3v-17l-10.6-.3-10.6-.3-2.5 3.3zm10.4 2.5c1.7 1.5 1.9 1.5 3.8-.2 2.6-2.4 3.9-1.1 1.5 1.5-1.7 1.9-1.7 2.1 0 4 2.4 2.6 1.1 3.9-1.5 1.5-1.9-1.7-2.1-1.7-4 0-2.6 2.4-3.9 1.1-1.5-1.5 1.7-1.9 1.7-2.1.2-3.8-3-3.3-1.8-4.5 1.5-1.5z"/>
-      </svg>
+      <BackspaceIcon />
     </Button>
 
     const Row = (props: { row: string[] }) => <div className="row">
       {props.row.map(
         (letter) => {
-          let color = 'darkgray'
+          let disabled = false;
           if (!['Enviar', 'Borrar'].includes(letter) && this.isLetterSent(letter.toUpperCase())) {
             if (!TODAY_WORD.includes(letter.toUpperCase())) {
-              color = 'white'
+              disabled = true
             }
           }
 
@@ -193,7 +208,7 @@ class DefaultView extends Component {
             return <DeleteButton></DeleteButton>
           }
 
-          return <KeyboardButton letter={letter} color={color}></KeyboardButton>
+          return <KeyboardButton letter={letter} disabled={disabled}></KeyboardButton>
         }
       )}
     </div>
@@ -239,11 +254,34 @@ class DefaultView extends Component {
       {sent.length === 5 && (<div style={{margin: '1rem auto', width: '100%', textAlign: 'center'}}>
         <p>La palabra era: <b>{TODAY_WORD}</b></p>
       </div>)}
-      {/*<h4 style={{margin: '1rem auto', width: '100%', textAlign: 'center'}}>{TODAY_WORD}</h4>*/}
+      {<h4 style={{margin: '1rem auto', width: '100%', textAlign: 'center'}}>{TODAY_WORD}</h4>}
       <div className="board">
         <Mesh elements={mesh}/>
       </div>
-      &nbsp;
+      <div className="info">
+        {notValidWord && 
+        <Alert 
+        action={
+          <IconButton color="primary" aria-label="limpiar palabra" component="span"
+            onClick={this.clearCurrentWord.bind(this)}>
+            <ClearAllIcon />
+          </IconButton>
+        }
+        severity="warning">
+          La palabra no está en el diccionario
+        </Alert>}
+        { solved &&
+        <Alert 
+        action={
+          <IconButton color="primary" aria-label="limpiar palabra" component="span"
+            onClick={() => {window.location.reload();}}>
+            <ReplaySharpIcon sx={{ fontSize: 20 }} />
+          </IconButton>
+        }
+        severity="success">
+          Enhorabuena, ha acertado la palabra
+        </Alert>}
+      </div>
       <div className="keyboard">
         <Row row={['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']}/>
         <Row row={['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ñ']}/>
--- src/Framework/index.tsx
+++ src/Framework/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import RouteService from "./Service/RouteService";
 import './style.sass';
 
+const myTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#000',
+      },
+    },
+  });
+
 ReactDOM.render(
-    <BrowserRouter>
-        <RouteService/>
-    </BrowserRouter>,
+    <ThemeProvider theme={myTheme}>
+        <BrowserRouter>
+            <RouteService/>
+        </BrowserRouter>
+    </ThemeProvider>,
     document.getElementById('app')
 );
